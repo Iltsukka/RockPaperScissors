7f036f45f2d353b5d62cdc530fb6fc3d6e006610
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-04-26 10:16:56 +0300

    Added playRound function

--- javascript.js
+++ javascript.js
@@ -1,9 +1,34 @@
 const options = ["Rock", "Paper", "Scissors"]
+let playerSelection = prompt("Choose rock, paper or scissors.").toLowerCase()
+const computerSelection = getComputerChoice()
 
 
-function getComputerChoice(options) {
+function getComputerChoice() {
     let choice = options[Math.floor(Math.random()*options.length)]
-    console.log(choice)
+    return choice
 }
 
-getComputerChoice(options)
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock."
+    } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors."
+    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beats Paper."
+    } else if (playerSelection === "paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock."
+    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beats Paper."
+    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors."
+    } else if (playerSelection === computerSelection.toLowerCase()) {
+        return "It is a tie!"
+    } else {
+        return "Please type a valid input"
+    }
+        
+}
+
+console.log(playRound(playerSelection, computerSelection))
